bf2cb910475d997844b8c232425247937fe7bd50
shawn-chambers <chambers.shawn.89@gmail.com>
2020-11-23 17:18:51 -0800

    seperate scripts for webpack prod vs dev

--- service1/webpack.config.js
+++ service1/webpack.config.js
@@ -1,15 +1,21 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve('client/app.jsx'),
+  entry: path.resolve('client/app.js'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/template.html')
+    })
+  ],
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
