8d2a9e6f1a30259197e52cb69f53407ec027041f
shawn-chambers <chambers.shawn.89@gmail.com>
2021-01-21 10:31:53 -0800

    init for server and db structure

---
+++ service2/server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use('/:id', express.static('public'));
+
+
+
+app.listen(port, () => { console.log(`carousel service listening on port ${port}`) })
