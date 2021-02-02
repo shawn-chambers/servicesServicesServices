dfcf91765b7ad8155b4d40f79bc879b34dd3526d
shawn-chambers <chambers.shawn.89@gmail.com>
2021-02-01 16:32:35 -0800

    sequelize connection to pg

---
+++ service3/db/index.js
@@ -0,0 +1,69 @@
+const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB, process.env.DBUSERNAME, null, {
+  host: process.env.DBHOST,
+  dialect: 'postgres'
+})
+
+sequelize.authenticate()
+  .then(() => console.log(`connected to db: ${process.env.DB}`))
+  .catch((err) => console.error(`error connecting to db ${process.env.DB}:`, err));
+
+
+const Review = sequelize.define('Review', {
+  review_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  review_date: {
+    type: DataTypes.DATE
+  },
+  rating: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  review_text: {
+    type: DataTypes.TEXT
+  },
+  header: {
+    type: DataTypes.TEXT
+  },
+  would_recommend: {
+    type: DataTypes.BOOLEAN
+  }
+})
+
+const User = sequelize.define('User', {
+  user_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  username: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  thumbnail: {
+    type: DataTypes.STRING
+  }
+});
+
+Review.belongsTo(User);
+User.hasMany(Review);
+
+Review.sync();
+User.sync();
+
+
+module.exports = {
+  db: sequelize,
+  User,
+  Review
+}
--- service3/server/app.js
+++ service3/server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require('../db/index');
 const morgan = require('morgan');
 
 app.use(morgan('dev'));
